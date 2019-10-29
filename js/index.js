995d46aad3265f39b55ac46c999990e9ac836de2
yutveg <kodi.0n31l@gmail.com>
2019-10-29 13:42:44 -0700

    initial poosh

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+//Header Image Skew and Unskew Events
+const headerimg = document.querySelector('header img');
+headerimg.addEventListener('mouseenter', () => {
+    headerimg.style.transform="skew(30deg, 20deg)";
+})
+
+headerimg.addEventListener('mouseleave', () => {
+    headerimg.style.transform="";
+})
+
+//Nav Flippy 
+const body = document.querySelector('body');
+const navitems = document.querySelectorAll('.main-navigation nav a');
+console.log(navitems);
+body.addEventListener('scroll', () => {
+    navitems.forEach(obj => {
+        obj.style.fontSize="50px";
+
+    })
+})
