5045d48a9f83ed6bb4629394b0b2aebb0e1f7eac
yutveg <kodi.0n31l@gmail.com>
2019-10-29 15:26:32 -0700

    completo

--- js/index.js
+++ js/index.js
@@ -1,22 +1,86 @@
 // Your code goes here
 
-//Header Image Skew and Unskew Events
-const headerimg = document.querySelector('header img');
-headerimg.addEventListener('mouseenter', () => {
-    headerimg.style.transform="skew(30deg, 20deg)";
+//Mouse Enter
+const headerImg = document.querySelector('header img');
+headerImg.addEventListener('mouseenter', () => {
+    headerImg.style.transform="skew(30deg, 20deg)";
 })
 
-headerimg.addEventListener('mouseleave', () => {
-    headerimg.style.transform="";
+//Mouse Leave
+headerImg.addEventListener('mouseleave', () => {
+    headerImg.style.transform="";
 })
 
-//Nav Flippy 
-const body = document.querySelector('body');
-const navitems = document.querySelectorAll('.main-navigation nav a');
-console.log(navitems);
-body.addEventListener('scroll', () => {
-    navitems.forEach(obj => {
-        obj.style.fontSize="50px";
+//Scroll 
+const imgItems = document.querySelectorAll('.content-section img');
+console.log(imgItems);
+window.addEventListener('scroll', () => {
+    imgItems.forEach( 
+        function(currentValue) { 
+          currentValue.style.transform="rotate(0.5turn)"; 
+        }
+      );
+})
 
-    })
+//Double Click
+const mainContent = document.querySelector('.content-section');
+const mainContentInverse = document.querySelector('.inverse-content')
+mainContent.addEventListener('dblclick', () => {
+    mainContent.style.flexDirection="row-reverse";
+    mainContentInverse.style.flexDirection="row-reverse";
+})
+mainContentInverse.addEventListener('dblclick', () => {
+    mainContent.style.flexDirection="row-reverse";
+    mainContentInverse.style.flexDirection="row-reverse";
+})
+
+
+//Click
+mainContent.addEventListener('click', () => {
+    imgItems.forEach( 
+        function(currentValue) { 
+          currentValue.style.transform=""; 
+        }
+      );
+})
+mainContentInverse.addEventListener('click', () => {
+    imgItems.forEach( 
+        function(currentValue) { 
+          currentValue.style.transform=""; 
+        }
+      );
+})
+
+//Wheel
+const bod = document.querySelector('body');
+const navHeader = document.querySelector('.main-navigation');
+console.log(navHeader);
+bod.addEventListener('wheel', () => {
+    navHeader.style.position="absolute";
+    navHeader.style.top="190%";
+    navHeader.style.transform="rotate(0.5turn)";
+})
+
+//Context
+navHeader.addEventListener('contextmenu', () => {
+    navHeader.style.backgroundImage="linear-gradient(319deg, #ffbc42 0%, #28965a 37%, #ee6352 100%)";
+})
+
+//Mouseover
+const brandName = document.querySelector('.main-navigation h1')
+brandName.addEventListener('mouseover', () => {
+    navHeader.style.position="fixed";
+    navHeader.style.top="0";
+})
+
+//Drag
+const lastImg = document.querySelector('.content-destination img');
+lastImg.addEventListener('drag', () => {
+    lastImg.style.width="100%";
+    navHeader.style.transform="";
+})
+
+//Auxclick
+bod.addEventListener('auxclick', () => {
+    bod.style.backgroundImage="linear-gradient(319deg, #e4ff6d 0%, #ffad42 37%, #e4ff6d 100%)";
 })
